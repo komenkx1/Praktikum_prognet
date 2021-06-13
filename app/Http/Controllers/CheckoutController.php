2ff8d2c36ef41f804f340a2ff3a0ff5c479afd75
komenkx <komangpermana7@gmail.com>
2021-06-13 16:59:36 +0800

    add method initPaymentGateaway to other controller

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -25,6 +25,16 @@ use Midtrans\Transaction;
 
 class CheckoutController extends Controller
 {
+       public function initPaymentGateaway(){
+        // Set your Merchant Server Key
+\Midtrans\Config::$serverKey = env("MIDTRANS_SERVER_KEY");
+// Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+\Midtrans\Config::$isProduction = false;
+// Set sanitization on (default)
+\Midtrans\Config::$isSanitized = true;
+// Set 3DS transaction for credit card to true
+\Midtrans\Config::$is3ds = true;
+    }
     /**
      * Display a listing of the resource.
      *
